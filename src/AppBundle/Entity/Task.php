6fd0e1692da469274cff2106ce12fd3d15e85fc4
l.vallet <luc.vallet@cpam-loireatlantique.cnamts.fr>
2017-03-09 08:58:28 +0100

    first outside class

---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// src/AppBundle/Entity/Task.php
+namespace AppBundle\Entity;
+
+class Task
+{
+    protected $task;
+    protected $dueDate;
+
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    public function setTask($task)
+    {
+        $this->task = $task;
+    }
+
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTime $dueDate = null)
+    {
+        $this->dueDate = $dueDate;
+    }
+}
