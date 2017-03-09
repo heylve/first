3231010b8df79a966e8f9358542e2080cbc1f785
l.vallet <luc.vallet@cpam-loireatlantique.cnamts.fr>
2017-03-09 11:43:40 +0100

    first round doctrine

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// src/AppBundle/Entity/Product.php
+namespace AppBundle\Entity;
+
+class Product
+{
+    private $name;
+    private $price;
+    private $description;
+}
