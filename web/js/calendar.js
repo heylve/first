db32f41c16210ec76c753f00f320233cd3af3e4c
l.vallet <luc.vallet@cpam-loireatlantique.cnamts.fr>
2017-04-12 10:37:17 +0200

    select smiley per day

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -3,25 +3,25 @@
         console.log( "document loaded" );
       
 });
-  $(document).on('click',"[id^=btn_mood]", function () {
+  $(document).on('click',"[id^=btn_mood]", function (event) {
+//              alert (var day=event.target.id;event.target.id);
+              var day_clicked=event.target.id;
+             $('#myModal').attr('data-id', day_clicked);  
+//             $(this).data('id')=event.target.id;
               $('#myModal').modal('show');
     }); 
-  $(document).on('click', '[id^=choose_smiley]', function(){
-    // what you want to happen when mouseover and mouseout 
-    // occurs on elements that match '.dosomething'
-    alert ("smiley clicked");
-});  
-  $("[id^=choose_smiley]").on ("click", function (){
-//       alert ($(this).id);
-       alert ("smiley clicked");
-//       $(this).attr('src', '/images/index.jpg');
-
-  });  
+  $(document).on('click', '[id^=choose_smiley]', function(event){
     
-// function notify() {
-//  alert( "clicked smiley" );
-//}
-//$( "#smiley" ).on( "click", notify );   
-//    $( "#dataTable tbody tr" ).on( "click", function() {
-//  console.log( $( this ).text() );
-//});
+//    alert ("Day "+day_clicked);
+    var btn_day=$('#myModal').attr('data-id');
+//    alert ("btn_day"+$('#myModal').attr('data-id'));
+//    alert ("btn_day "+btn_day);  
+    var day_clicked= parseInt(btn_day.match(/\d+$/)[0], 10);
+    var smiley_chosen = parseInt(event.target.id.match(/\d+$/)[0], 10);
+//    alert ("smiley_chosen".smiley_chosen);
+//    alert ("day_clicked"+day_clicked);
+    $("#smiley_"+day_clicked).attr('src','/images/smiley_'+smiley_chosen+'.png');
+    
+});  
+ 
+    
