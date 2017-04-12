9a5acd4f4bb0047064854bc5fadb171eb83f03ea
l.vallet <luc.vallet@cpam-loireatlantique.cnamts.fr>
2017-04-12 14:15:36 +0200

    clic sans button

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -3,7 +3,7 @@
         console.log( "document loaded" );
       
 });
-  $(document).on('click',"[id^=btn_mood]", function (event) {
+  $(document).on('click',"[id^=btn_mood] , .p_day", function (event) {
 //              alert (var day=event.target.id;event.target.id);
               var day_clicked=event.target.id;
              $('#myModal').attr('data-id', day_clicked);  
@@ -15,13 +15,14 @@
 //    alert ("Day "+day_clicked);
     var btn_day=$('#myModal').attr('data-id');
 //    alert ("btn_day"+$('#myModal').attr('data-id'));
-//    alert ("btn_day "+btn_day);  
+//      alert ("btn_day "+btn_day);  
     var day_clicked= parseInt(btn_day.match(/\d+$/)[0], 10);
     var smiley_chosen = parseInt(event.target.id.match(/\d+$/)[0], 10);
 //    alert ("smiley_chosen".smiley_chosen);
 //    alert ("day_clicked"+day_clicked);
     $("#smiley_"+day_clicked).attr('src','/images/smiley_'+smiley_chosen+'.png');
-    
+     $('#myModal').modal('hide');
+     
 });  
  
     
